92f5f81a95c5176a6c36dc2434304b5fa3750e52
naira_tamer <naira.othman.Tamer@gmail.com>
2026-08-22 16:50:48 +0300

    Set Models

---
+++ Core/Domain/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Models;
+
+public class Product : BaseEntity<int>  {
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string PictureUrl { get; set; } = default!;
+    public decimal Price { get; set; }    
+    public int BrandId { get; set; }
+    public ProductBrand ProductBrand { get; set; }
+    public int TypeId { get; set; }
+    public ProductType ProductType { get; set; }
+}
---
+++ Core/Domain/Models/ProductBrand.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Domain.Models;
+
+public class ProductBrand : BaseEntity<int>
+{
+    public string Name { get; set; } = default!;
+    public ICollection<Product> Products { get; set; } 
+}
